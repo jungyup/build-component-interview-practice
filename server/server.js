951e48d1ca0c72bea9fe93956886c7b11928daeb
jungyup <jungyupkim4@gmail.com>
2022-07-14 23:54:41 -0400

    add package.json for server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 const port = 3000; // changing this value and restarting the server will change the port on which the app listens
 
+const getRandomIndexValue = (list) => {
+    return list[Math.floor(Math.random() * list.length)];
+};
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
